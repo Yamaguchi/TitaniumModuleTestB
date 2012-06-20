2f6f5354bcc9cae3f824677342d48196eb58f2d9
H_Yamaguchi <gen.yamaguchi0@gmail.com>
2012-06-20 10:43:46 +0900

    initial

---
+++ Classes/ModuleTestBModuleAssets.m
@@ -0,0 +1,15 @@
+/**
+ * This is a generated file. Do not edit or your changes will be lost
+ */
+#import "ModuleTestBModuleAssets.h"
+
+extern NSData* filterData(NSString* thedata, NSString* identifier);
+
+@implementation ModuleTestBModuleAssets
+
+- (NSData*) moduleAsset
+{
+	return nil;
+}
+
+@end
---
+++ build/moduletestb.build/Release-iphoneos/moduletestb.build/Objects-normal/armv7/ModuleTestBModuleAssets.d
@@ -0,0 +1,3 @@
+dependencies: \
+  /Users/h_yamaguchi/Projects/repository/mcf/touch/ModuleTestB/Classes/ModuleTestBModuleAssets.m \
+  /Users/h_yamaguchi/Projects/repository/mcf/touch/ModuleTestB/Classes/ModuleTestBModuleAssets.h
